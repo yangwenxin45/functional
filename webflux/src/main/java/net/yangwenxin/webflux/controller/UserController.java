e8568eb0cce71e6d00efe1e100bcccf9d84a0f62
yangwenxin <yangwenxin45@gmail.com>
2019-05-23 20:10:13 +0800

    WebFlux继承MongoDB

--- webflux/src/main/java/net/yangwenxin/webflux/controller/UserController.java
+++ webflux/src/main/java/net/yangwenxin/webflux/controller/UserController.java
@@ -2,11 +2,12 @@ package net.yangwenxin.webflux.controller;
 
 import net.yangwenxin.webflux.domain.User;
 import net.yangwenxin.webflux.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/user")
@@ -34,5 +35,103 @@ public class UserController {
     @GetMapping(value = "/stream/all", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<User> streamGetAll() {
         return repository.findAll();
+    }
+
+    /**
+     * 新增数据
+     *
+     * @param user
+     * @return
+     */
+    @PostMapping("/")
+    public Mono<User> createUser(@RequestBody User user) {
+        // spring data jpa 里面，新增和修改都是save，有id是修改，id为空是新增
+        // 根据实际情况是否置空id
+        user.setId(null);
+        return this.repository.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Void>> deleteUser(@PathVariable("id") String id) {
+//        deleteById没有返回值，不能判断数据是否存在
+//        this.repository.deleteById(id);
+        return this.repository.findById(id)
+                // 当你要操作数据，并返回一个Mono这个时候使用flatMap
+                // 如果不操作数据，只是转换数据，使用map
+                .flatMap(user -> this.repository.delete(user)
+                        .then(Mono.just(new ResponseEntity<Void>(HttpStatus.OK))))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    /**
+     * 修改数据
+     * 存在的时候返回200和修改后的数据，不存在的时候返回404
+     *
+     * @param id
+     * @param user
+     * @return
+     */
+    @PutMapping("/{id}")
+    public Mono<ResponseEntity<User>> updateUser(
+            @PathVariable("id") String id,
+            @RequestBody User user
+    ) {
+        return this.repository.findById(id)
+                // flatMap操作数据
+                .flatMap(u -> {
+                    u.setAge(user.getAge());
+                    u.setName(user.getName());
+                    return this.repository.save(u);
+                })
+                // map 转换数据
+                .map(u -> new ResponseEntity<User>(u, HttpStatus.OK))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    /**
+     * 根据ID查找用户
+     * 存在返回用户信息，不存在返回404
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<User>> findUserById(
+            @PathVariable("id") String id
+    ) {
+        return this.repository.findById(id)
+                .map(u -> new ResponseEntity<User>(u, HttpStatus.OK))
+                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    /**
+     * 根据年龄查找用户
+     *
+     * @param start
+     * @param end
+     * @return
+     */
+    @GetMapping("/age/{start}/{end}")
+    public Flux<User> findByAge(@PathVariable("start") int start,
+                                @PathVariable("end") int end) {
+        return this.repository.findByAgeBetween(start, end);
+    }
+
+    /**
+     * 根据年龄查找用户
+     *
+     * @param start
+     * @param end
+     * @return
+     */
+    @GetMapping(value = "/stream/age/{start}/{end}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<User> streamFindByAge(@PathVariable("start") int start,
+                                      @PathVariable("end") int end) {
+        return this.repository.findByAgeBetween(start, end);
+    }
+
+    @GetMapping("/old")
+    public Flux<User> oldUser() {
+        return this.repository.oldUser();
     }
 }
--- webflux/src/main/java/net/yangwenxin/webflux/domain/User.java
+++ webflux/src/main/java/net/yangwenxin/webflux/domain/User.java
@@ -1,8 +1,11 @@
 package net.yangwenxin.webflux.domain;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
 
 @Document(collection = "user")
 @Data
@@ -11,7 +14,9 @@ public class User {
     @Id
     private String id;
 
+    @NotBlank
     private String name;
 
+    @Range(min = 10, max = 100)
     private int age;
 }
